372bc7bfe16b64deab7d27c044638b93cd502e55
Keshav Naganathan <50137135+Beginner-Kesh@users.noreply.github.com>
2020-06-26 19:21:17 +0530

    Add files via upload
    Added files to the Sending message repo

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fast2sms = require('fast-two-sms');
+
+const app = express();
+
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+//Index route
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.post('/sendMessage', async (req, res) => {
+  const response = await fast2sms.sendMessage({
+    authorization: process.env.API_KEY,
+    message: req.body.message,
+    numbers: [req.body.number],
+  });
+  res.send(response);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
